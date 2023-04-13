6b18579ce1d49e46efbf7be01be1915c20648598
Petro Strynada <petro.strynada@gmail.com>
2023-04-13 20:54:11 +0300

    Filter logic for sections in grid view

--- LittleLemonDinnerMenu/LittleLemonDinnerMenu/Screens/Menultems/GridView.swift
+++ LittleLemonDinnerMenu/LittleLemonDinnerMenu/Screens/Menultems/GridView.swift
@@ -9,48 +9,140 @@ import SwiftUI
 
 struct GridView: View {
     @EnvironmentObject var menu: Menu
+    @EnvironmentObject var menuOption: MenuOption
     
     let columns = [
         GridItem(.adaptive(minimum: 115), alignment: .top) //for 3 line grid
     ]
     
-    enum menuCategories {
-        case Food, Drink, Dessert
-    }
-    
     var body: some View {
         ScrollView {
             LazyVGrid(columns: columns, pinnedViews: .sectionHeaders) {
                 ForEach(menu.sections) { section in
-                    Section {
-                        ForEach(section.meal) { meal in
-                            NavigationLink {
-                                MenuItemDetailsView(meal: meal)
-                            } label: {
-                                VStack {
-                                    Image(meal.image)
-                                        .imageStyle()
-                                        .cornerRadius(10)
-                                    
-                                    Text(meal.name)
-                                        .bodyStyle()
-                                        .multilineTextAlignment(.center)
-                                        .frame(minWidth: 117) //so that the word "Mediterranean" fits on one line without wrapping the word in a grid with three lines
+                    
+                    if menuOption.sectionFood == true && section.name == "Food" {
+                        Section {
+                            ForEach(section.meal) { meal in
+                                NavigationLink {
+                                    MenuItemDetailsView(meal: meal)
+                                } label: {
+                                    VStack {
+                                        Image(meal.image)
+                                            .imageStyle()
+                                            .cornerRadius(10)
+                                        
+                                        Text(meal.name)
+                                            .bodyStyle()
+                                            .multilineTextAlignment(.center)
+                                            .frame(minWidth: 117) //so that the word "Mediterranean" fits on one line without wrapping the word in a grid with three lines
+                                    }
+                                    .padding(.bottom)
                                 }
-                                .padding(.bottom)
                             }
+                        } header: {
+                            Text(section.name)
+                                .sectionHeaderStyle()
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.top, .bottom, .trailing], 5)
+                                .background(.background)
                         }
-                    } header: {
-                        Text(section.name)
-                            .sectionHeaderStyle()
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding([.top, .bottom, .trailing], 5)
-                            .background(.background)
                     }
+                    
+                    if menuOption.sectionDrink == true && section.name == "Drinks" {
+                        Section {
+                            ForEach(section.meal) { meal in
+                                NavigationLink {
+                                    MenuItemDetailsView(meal: meal)
+                                } label: {
+                                    VStack {
+                                        Image(meal.image)
+                                            .imageStyle()
+                                            .cornerRadius(10)
+                                        
+                                        Text(meal.name)
+                                            .bodyStyle()
+                                            .multilineTextAlignment(.center)
+                                            .frame(minWidth: 117) //so that the word "Mediterranean" fits on one line without wrapping the word in a grid with three lines
+                                    }
+                                    .padding(.bottom)
+                                }
+                            }
+                        } header: {
+                            Text(section.name)
+                                .sectionHeaderStyle()
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.top, .bottom, .trailing], 5)
+                                .background(.background)
+                        }
+                    }
+                    
+                    if menuOption.sectionDesserts == true && section.name == "Dessert" {
+                        Section {
+                            ForEach(section.meal) { meal in
+                                NavigationLink {
+                                    MenuItemDetailsView(meal: meal)
+                                } label: {
+                                    VStack {
+                                        Image(meal.image)
+                                            .imageStyle()
+                                            .cornerRadius(10)
+                                        
+                                        Text(meal.name)
+                                            .bodyStyle()
+                                            .multilineTextAlignment(.center)
+                                            .frame(minWidth: 117) //so that the word "Mediterranean" fits on one line without wrapping the word in a grid with three lines
+                                    }
+                                    .padding(.bottom)
+                                }
+                            }
+                        } header: {
+                            Text(section.name)
+                                .sectionHeaderStyle()
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.top, .bottom, .trailing], 5)
+                                .background(.background)
+                        }
+                    }
+                    
+                    if menuOption.allSections == true {
+                        Section {
+                            ForEach(section.meal) { meal in
+                                NavigationLink {
+                                    MenuItemDetailsView(meal: meal)
+                                } label: {
+                                    VStack {
+                                        Image(meal.image)
+                                            .imageStyle()
+                                            .cornerRadius(10)
+                                        
+                                        Text(meal.name)
+                                            .bodyStyle()
+                                            .multilineTextAlignment(.center)
+                                            .frame(minWidth: 117) //so that the word "Mediterranean" fits on one line without wrapping the word in a grid with three lines
+                                    }
+                                    .padding(.bottom)
+                                }
+                            }
+                        } header: {
+                            Text(section.name)
+                                .sectionHeaderStyle()
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.top, .bottom, .trailing], 5)
+                                .background(.background)
+                        }
+                    }
+
+                    
+                    
                 }
             }
             .padding(.horizontal)
         }
+    }
+    
+    func checkMark(menuOptionSection: Bool) -> Image {
+        guard menuOptionSection else { return Image("") }
+        return Image(systemName: "checkmark")
     }
 }
 
@@ -58,5 +150,8 @@ struct GridView_Previews: PreviewProvider {
     static var previews: some View {
         GridView()
             .environmentObject(Menu())
+            .environmentObject(MenuOption())
     }
 }
+
+
